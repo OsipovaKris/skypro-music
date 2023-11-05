25e157782a36da7f61bdd2d03eb2d59226fad0ca
OsipovaKris <Osipova.svs@mail.ru>
2023-11-05 17:19:05 +0300

    добавлен компонент Sidebar

--- skypro-music/src/App.js
+++ skypro-music/src/App.js
@@ -1,5 +1,6 @@
 // import "./App.css";
 import { NavMenu } from "./components/NavMenu";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -611,47 +612,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="main__sidebar sidebar">
-          <div className="sidebar__personal">
-            <p className="sidebar__personal-name">Sergey.Ivanov</p>
-            <div className="sidebar__icon">
-              <svg alt="logout">
-                <use xlinkHref="img/icon/sprite.svg#logout"></use>
-              </svg>
-            </div>
-          </div>
-          <div className="sidebar__block">
-            <div className="sidebar__list">
-              <div className="sidebar__item">
-                <a className="sidebar__link" href="#">
-                  <img
-                    className="sidebar__img"
-                    src="img/playlist01.png"
-                    alt="day's playlist"
-                  ></img>
-                </a>
-              </div>
-              <div className="sidebar__item">
-                <a className="sidebar__link" href="#">
-                  <img
-                    className="sidebar__img"
-                    src="img/playlist02.png"
-                    alt="day's playlist"
-                  ></img>
-                </a>
-              </div>
-              <div className="sidebar__item">
-                <a className="sidebar__link" href="#">
-                  <img
-                    className="sidebar__img"
-                    src="img/playlist03.png"
-                    alt="day's playlist"
-                  ></img>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Sidebar />
       </main>
       <div className="bar">
         <div className="bar__content">
