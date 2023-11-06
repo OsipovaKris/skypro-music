42e1394c3b1f8c5514f9fc7a7f3e04b631970601
OsipovaKris <Osipova.svs@mail.ru>
2023-11-06 19:01:37 +0300

    Добавлен компонент Filter

--- skypro-music/src/components/Tracklist.js
+++ skypro-music/src/components/Tracklist.js
@@ -1,4 +1,5 @@
 import { Search } from "./Search";
+import { Filter } from "./Filter";
 import { Track } from "./Track";
 
 export function Tracklist() {
@@ -6,14 +7,7 @@ export function Tracklist() {
     <div className="main__centerblock centerblock">
       <Search />
       <h2 className="centerblock__h2">Треки</h2>
-      <div className="centerblock__filter filter">
-        <div className="filter__title">Искать по:</div>
-        <div className="filter__button button-author _btn-text">
-          исполнителю
-        </div>
-        <div className="filter__button button-year _btn-text">году выпуска</div>
-        <div className="filter__button button-genre _btn-text">жанру</div>
-      </div>
+      <Filter />
       <div className="centerblock__content">
         <div className="content__title playlist-title">
           <div className="playlist-title__col col01">Трек</div>
