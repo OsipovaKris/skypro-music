d3372decec0cd57df8ff608aa49dcf79d98b42f3
OsipovaKris <Osipova.svs@mail.ru>
2023-11-06 18:50:48 +0300

    Добавлен компонент Search

--- skypro-music/src/components/Tracklist.js
+++ skypro-music/src/components/Tracklist.js
@@ -1,19 +1,10 @@
+import { Search } from "./Search";
 import { Track } from "./Track";
 
 export function Tracklist() {
   return (
     <div className="main__centerblock centerblock">
-      <div className="centerblock__search search">
-        <svg className="search__svg">
-          <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-        </svg>
-        <input
-          className="search__text"
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
+      <Search />
       <h2 className="centerblock__h2">Треки</h2>
       <div className="centerblock__filter filter">
         <div className="filter__title">Искать по:</div>
